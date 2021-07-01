c4ceabdd8be1eaf0b6c58059e9a9d17cbfd8aa95
RAGAdox <rishitech882@gmail.com>
2021-07-01 10:35:55 +0530

    SHOW STATUS AND IMPLEMENT LOCK

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const path = require("path");
 const app = express();
 let led1 = false;
 let led2 = false;
+let locked = false;
 //Middleware to set a static directory
 app.use(express.static(path.join(__dirname, "public")));
 /*
   We are creating end points to access the current values of the variables led1 and led2
 */
 app.get("/api/status_text", (req, res) => {
+  if (locked) locked = !locked;
   return res.send(led1 + "," + led2);
 });
 app.get("/api/status_json", (req, res) => {
@@ -17,13 +19,27 @@ app.get("/api/status_json", (req, res) => {
 });
 // End point to change the value of led1
 app.get("/api/led1", (req, res) => {
-  led1 = !led1;
-  return res.send(led1 + "," + led2);
+  if (!locked) {
+    led1 = !led1;
+    locked = true;
+    return res.json({ led1: led1, led2: led2 });
+  } else {
+    return res.json({
+      error: "Application is locked for furthur modifications to be made",
+    });
+  }
 });
 // End point to change the value of led2
 app.get("/api/led2", (req, res) => {
-  led2 = !led2;
-  return res.send(led1 + "," + led2);
+  if (!locked) {
+    led2 = !led2;
+    locked = true;
+    return res.json({ led1: led1, led2: led2 });
+  } else {
+    return res.json({
+      error: "Application is locked for furthur modifications to be made",
+    });
+  }
 });
 // SEND index.html to the browser when / is requested
 app.get("/", (req, res) => {
